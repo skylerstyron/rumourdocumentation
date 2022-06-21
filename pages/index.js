a9a91dadd77e3eba13df2d866bb3b49f2d00f46c
skylerstyron <skylerstyrondev@gmail.com>
2022-06-21 10:21:50 -0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,66 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Rumour Media Docs</title>
+        <meta name="description" content="Internal documentation for Rumour Media" />
+        <link rel="icon" href="/icon.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <h2>Welcome to</h2>
+        <h1 className={styles.title}>
+          <a href="https://rumourmedia.com">RUM<span className={styles.logoIcon}><Image src="/icon.png" alt="Vercel Logo" width={70} height={70} /></span>UR MEDIA</a>
+        </h1>
+
+        <p className={styles.description}>
+          {/* Get started by editing{' '}<code className={styles.code}>pages/index.js</code> */}
+        </p>
+
+        <div className={styles.grid}>
+          <Link href="/monday">
+            <a className={styles.card}>
+              <h2>Project Management  &rarr;</h2>
+              <p>How we manage tasks, update projects, and track time.</p>
+            </a>
+          </Link>
+
+          <Link href="/">
+            <a className={styles.card}>
+              <h2>Our Clients &rarr;</h2>
+              <p>Learn about the clients we work with and thier brand image.</p>
+            </a>
+          </Link>
+
+          <Link href="/">
+            <a className={styles.card}>
+              <h2>Our Team &rarr; </h2>
+              <p>Get to know our team and who does what.</p>
+            </a>
+          </Link>
+
+          <Link href="/">
+            <a className={styles.card}>
+              <h2>?</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </a>
+          </Link>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <p>
+          Powered by {' '}
+          <code className={styles.code}>brain wrinkles</code>
+        </p>
+      </footer>
+    </div>
+  )
+}
---
+++ pages/monday.js
@@ -0,0 +1,68 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Monday.module.css'
+import Link from 'next/link'
+
+export default function Monday() {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Rumour Media Docs | Monday.com</title>
+                <meta name="description" content="How we manage projects at Rumour Media." />
+                <link rel="icon" href="/icon.png" />
+            </Head>
+
+            <Link href="/">
+                <a className={styles.backtohome}><code className={styles.code}>back to home</code></a>
+            </Link>
+            <main className={styles.main}>
+                <div className={styles.wrapper}>
+                    <h1 className={styles.title}>
+                        Monday.com
+                    </h1>
+
+                    <div className={styles.description}>
+                        <h2 className={styles.sectionHead}>Navigation</h2>
+                    </div>
+
+                    <div className={styles.grid}>
+                        <Link href="/">
+                            <a className={styles.card}>
+                                <h2>?</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </a>
+                        </Link>
+
+                        <Link href="/">
+                            <a className={styles.card}>
+                                <h2>?</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </a>
+                        </Link>
+
+                        <Link href="/">
+                            <a className={styles.card}>
+                                <h2>?</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </a>
+                        </Link>
+
+                        <Link href="/">
+                            <a className={styles.card}>
+                                <h2>?</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+            </main>
+
+            <footer className={styles.footer}>
+                <p>
+                    Powered by {' '}
+                    <code className={styles.code}>brain wrinkles</code>
+                </p>
+            </footer>
+        </div>
+    )
+}

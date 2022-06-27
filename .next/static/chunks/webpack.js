c24d5f9c053899c883852722e008125988a1c3eb
skylerstyron <skylerstyrondev@gmail.com>
2022-06-27 15:58:57 -0500

    adding welcome to

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7bd734db48d43e7e"; }
+/******/ 		__webpack_require__.h = function() { return "a729afe273939d3b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
